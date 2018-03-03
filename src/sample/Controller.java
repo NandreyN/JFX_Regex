3c50c9165ea47797faa862f4400af7dc0b0571b4
Андрей <andrey.naumovich@mail.ru>
2018-03-03 14:53:54 +0300

    first task

---
+++ src/sample/Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableBooleanValue;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+
+import java.util.concurrent.Callable;
+
+public class Controller {
+    @FXML
+    private ComboBox<String> comboBox;
+    @FXML
+    private TextField textField;
+    @FXML
+    private ObservableList<String> comboBoxValues;
+    @FXML
+    private ImageView imageIndicator;
+
+    private BooleanProperty flag;
+    private Image success, error;
+    private Validator validator;
+
+    public Controller() {
+        flag = new SimpleBooleanProperty();
+        validator = new Validator();
+        error = new Image(getClass().getResourceAsStream("resources/error.png"));
+        success = new Image(getClass().getResourceAsStream("resources/success.png"));
+    }
+
+    @FXML
+    private void initialize() {
+        imageIndicator.imageProperty().bind(Bindings.when(flag).then(success).otherwise(error));
+
+        comboBoxValues = comboBox.getItems();
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            flag.setValue(validator.isValid(newValue, comboBox.getSelectionModel().getSelectedItem()));
+        });
+    }
+}
---
+++ src/sample/Validator.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.css.Match;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    private Map<String, Function<String, Boolean>> functionalMap;
+    private static final String NATURAL_REGEX_STRING = "([0]{1})|([1-9]\\d*)";
+    private static final String INTEGER_REGEX_STRING = "((-?)[1-9]\\d*)|([0]{1})";
+    private static final String FLOAT_REGEX_STRING = "[+-]?\\d*(([.]?\\d+)?)(([Ee][+-]?\\d*)?)";
+    private static final String DATE_REGEX_STRING = "(([0-9]{4})[-./]((1[012])|(0[1-9]))[-./]((0[1-9])|(1[0-9])|(2[0-9])|3[01]))|" +
+            "(((1[012])|(0[1-9]))[-./]((0[1-9])|(1[0-9])|(2[0-9])|3[01])[-./]([0-9]{4}))|" +
+            "(((0[1-9])|(1[0-9])|(2[0-9])|3[01])[-./]((1[012])|(0[1-9]))[-./]([0-9]{4}))"; // YYYY:MM:DD ; MM:DD:YYYY, DD:MM:YYYY
+    private static final String TIME_REGEX_STRING
+            = "(([01][0-9])|(2[0-3]))[:]([0-5][\\d{1}])[:]([0-5][\\d{1}])"; //HH:MI:SS
+    private static final String EMAIL_REGEX_STRING = "([a-zA-Z0-9_.]{5,})@([a-zA-Z]{3,6})[.]([a-z]{2,3})";
+
+    private Matcher matcher;
+    public Validator() {
+        functionalMap = new HashMap<>();
+        functionalMap.put("natural", (x) -> Pattern.compile(NATURAL_REGEX_STRING).matcher(x).matches());
+        functionalMap.put("integer", (x) -> Pattern.compile(INTEGER_REGEX_STRING).matcher(x).matches());
+        functionalMap.put("float", (x) -> Pattern.compile(FLOAT_REGEX_STRING).matcher(x).matches());
+        functionalMap.put("date", (x) -> Pattern.compile(DATE_REGEX_STRING).matcher(x).matches());
+        functionalMap.put("time", (x) -> Pattern.compile(TIME_REGEX_STRING).matcher(x).matches());
+        functionalMap.put("e-mail", (x) -> Pattern.compile(EMAIL_REGEX_STRING).matcher(x).matches());
+    }
+
+    public boolean isValid(String requestString, String mode) throws IllegalArgumentException {
+        if (!functionalMap.containsKey(mode.toLowerCase()))
+            throw new IllegalArgumentException("No such mode found");
+
+        return functionalMap.get(mode.toLowerCase()).apply(requestString);
+    }
+}
